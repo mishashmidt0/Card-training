ffe2bfdd05c4d6dafe9da3d26091c8299df959bb
PavelDermichev1998 <pdermichev@gmail.com>
2022-06-26 19:36:24 +0300

    Refactor Register component, add style for Register

--- src/n2-auth/a2-register/Register.tsx
+++ src/n2-auth/a2-register/Register.tsx
@@ -1,103 +1,18 @@
 import React from 'react';
-import { Formik } from 'formik';
 import s from './registerStyle.module.css';
-import { registerTC } from './register-reducer';
-import { AppRootStateType, useTypedDispatch } from '../../n10-bll/redux';
+import { AppRootStateType } from '../../n10-bll/redux';
 import { useSelector } from 'react-redux';
-import { Link, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { RegisterForm } from './RegisterForm';
 
 export const Register = () => {
   const register = useSelector<AppRootStateType>(state => state.register.isRegistered);
-  const dispatch = useTypedDispatch();
 
-  if (register) {
-    return <Navigate to="/login" />;
-  } else {
-    return (
-      <div className={s.registerContainer}>
-        <h1>Sing Up</h1>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-            repeatPassword: '',
-          }}
-          validate={values => {
-            const errors: Partial<
-              Omit<{ email: string; password: string; repeatPassword: string }, 'captcha'>
-            > = {};
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-              errors.email = 'Invalid email address';
-            }
-            if (!values.password) {
-              errors.password = 'Password is required';
-            } else if (values.password.length < 8) {
-              errors.password = 'The password must be more than 8 characters';
-            }
-            if (values.repeatPassword !== values.password) {
-              errors.repeatPassword = 'Password does not match';
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setSubmitting(false);
-            dispatch(registerTC({ email: values.email, password: values.password }));
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-          }) => (
-            <form onSubmit={handleSubmit} className={s.form}>
-              <input
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-                placeholder={'Email'}
-              />
-              {errors.email && touched.email && (
-                <div style={{ color: 'red' }}>{errors.email}</div>
-              )}
-              <input
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-                placeholder={'Password'}
-              />
-              {errors.password && touched.password && (
-                <div style={{ color: 'red' }}>{errors.password}</div>
-              )}
-              <input
-                type="password"
-                name="repeatPassword"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.repeatPassword}
-                placeholder={'Confirm password'}
-              />
-              {errors.repeatPassword && touched.repeatPassword && (
-                <div style={{ color: 'red' }}>{errors.repeatPassword}</div>
-              )}
-              <button type="submit" disabled={isSubmitting}>
-                REGISTER
-              </button>
-              Already have an account?
-              <Link to={'/login'}>Sing In</Link>
-            </form>
-          )}
-        </Formik>
-      </div>
-    );
-  }
+  if (register) return <Navigate to="/login" />;
+  return (
+    <div className={s.registerContainer}>
+      <h1>Sing Up</h1>
+      <RegisterForm />
+    </div>
+  );
 };
---
+++ src/n2-auth/a2-register/RegisterForm.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Formik } from 'formik';
+import s from './registerStyle.module.css';
+import { registerTC } from './register-reducer';
+import { useTypedDispatch } from '../../n10-bll/redux';
+import { Link } from 'react-router-dom';
+import { SuperInput } from '../a1-login/l1-components/SuperInput';
+import { Button } from '@mui/material';
+
+export const RegisterForm = () => {
+
+  const dispatch = useTypedDispatch();
+
+  return (
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+        repeatPassword: '',
+      }}
+      validate={values => {
+        const errors: Partial<Omit<{ email: string; password: string; repeatPassword: string }, 'captcha'>> = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+          errors.email = 'Invalid email address';
+        }
+        if (!values.password) {
+          errors.password = 'Password is required';
+        } else if (values.password.length < 8) {
+          errors.password = 'The password must be more than 8 characters';
+        }
+        if (values.repeatPassword !== values.password) {
+          errors.repeatPassword = 'Password does not match';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setSubmitting(false);
+        dispatch(registerTC({ email: values.email, password: values.password }));
+      }}
+    >
+      {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+        <form onSubmit={handleSubmit} className={s.form}>
+          <SuperInput
+            title={'Email'}
+            name={'email'}
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+            value={values.email}
+            error={!!errors.email}
+          />
+          {errors.email && touched.email && (
+            <div style={{ color: 'red' }}>{errors.email}</div>
+          )}
+          <SuperInput
+            title={'Password'}
+            name={'password'}
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+            value={values.password}
+            error={!!errors.password}
+          />
+          {errors.password && touched.password && (
+            <div style={{ color: 'red' }}>{errors.password}</div>
+          )}
+          <SuperInput
+            title={'Confirm password'}
+            name={'repeatPassword'}
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+            value={values.repeatPassword}
+            error={!!errors.repeatPassword}
+          />
+          {/*<input
+            type='password'
+            name='repeatPassword'
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.repeatPassword}
+            placeholder={'Confirm password'}
+          />*/}
+          {errors.repeatPassword && touched.repeatPassword && (
+            <div style={{ color: 'red' }}>{errors.repeatPassword}</div>
+          )}
+          <Button variant="contained" type="submit" disabled={isSubmitting}>
+            REGISTER
+          </Button>
+          Already have an account?
+          <Link to={'/login'}>Sing In</Link>
+        </form>
+      )}
+    </Formik>
+  );
+};
+
