80d2c439982bb436aa228bf2623273b5862d69fd
PavelDermichev1998 <pdermichev@gmail.com>
2022-07-04 20:31:55 +0300

    refactor eslint

--- src/n1-main/m0-App/App.tsx
+++ src/n1-main/m0-App/App.tsx
@@ -20,7 +20,6 @@ export const App = (): ReturnComponentType => {
     if (!isLoggedIn) dispatch(getUserProfileTC());
   }, []);
 
-  // asdasd
   return !globalLoading ? (
     <GlobalLoadingComponent />
   ) : (
