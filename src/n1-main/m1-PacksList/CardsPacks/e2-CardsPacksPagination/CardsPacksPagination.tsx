b803c7ea4aaa3ae3c4d26e370c2a4b37599b74d8
PavelDermichev1998 <pdermichev@gmail.com>
2022-07-07 21:14:30 +0300

    refactor: cardsPacks filter for update, cardsCount, disabled to request pagination

--- src/n1-main/m1-PacksList/CardsPacks/e2-CardsPacksPagination/CardsPacksPagination.tsx
+++ src/n1-main/m1-PacksList/CardsPacks/e2-CardsPacksPagination/CardsPacksPagination.tsx
@@ -4,13 +4,17 @@ import TablePagination from '@mui/material/TablePagination';
 
 import { ReturnComponentType } from '../../../../n4-types';
 import { useAppSelector, useTypedDispatch } from '../../../../n5-bll/redux';
+import { ProfileStateType } from '../../../m2-Profile/profile-reducer';
 import { changePageCount } from '../../p1-FilterComponent/filter-reducer';
+import { FilterText } from '../../p3-enums/enums';
 import { getCardsPacksTC } from '../cardsPacks-reducer';
 
 import s from './CardsPacksPagination.module.css';
 
 export const CardsPacksPagination = (): ReturnComponentType => {
-  const pageCount = useAppSelector(state => state.filter.pageCount);
+  // eslint-disable-next-line no-underscore-dangle
+  const userId = useAppSelector(state => (state.profile.profile as ProfileStateType)._id);
+  const filter = useAppSelector(state => state.filter);
   const loading = useAppSelector(state => state.app.loading);
   const [page, setPage] = React.useState(0);
 
@@ -23,17 +27,46 @@ export const CardsPacksPagination = (): ReturnComponentType => {
     event: React.MouseEvent<HTMLButtonElement> | null,
     newPage: number,
   ) => void = (event, newPage) => {
+    const payload =
+      filter.isShowCards === FilterText.my
+        ? { user_id: userId, pageCount: filter.pageCount, page: newPage + 1 }
+        : { pageCount: filter.pageCount, page: newPage + 1 };
+
     setPage(newPage);
-    dispatch(getCardsPacksTC({ pageCount, page: newPage + 1 }));
+    dispatch(getCardsPacksTC(payload));
   };
 
   const handleChangeRowsPerPage: (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => void = event => {
+    const payload =
+      filter.isShowCards === FilterText.my
+        ? { user_id: userId, pageCount: parseInt(event.target.value, 10) }
+        : { pageCount: parseInt(event.target.value, 10) };
+
     dispatch(changePageCount(parseInt(event.target.value, 10)));
     setPage(0);
-    dispatch(getCardsPacksTC({ pageCount: parseInt(event.target.value, 10) }));
+    dispatch(getCardsPacksTC(payload));
   };
+
+  if (!loading) {
+    return (
+      <TablePagination
+        component="div"
+        count={cardPacksTotalCount}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={filter.pageCount}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        className={s.paginatorContainer}
+        showLastButton
+        showFirstButton
+        SelectProps={{
+          disabled: loading,
+        }}
+      />
+    );
+  }
 
   return (
     <TablePagination
@@ -41,7 +74,7 @@ export const CardsPacksPagination = (): ReturnComponentType => {
       count={cardPacksTotalCount}
       page={page}
       onPageChange={handleChangePage}
-      rowsPerPage={pageCount}
+      rowsPerPage={filter.pageCount}
       onRowsPerPageChange={handleChangeRowsPerPage}
       className={s.paginatorContainer}
       showLastButton
